ddb673606d612f28fc40a14d9c747150f79199ef
lucienbertin <me@lucienbertin.com>
2018-06-26 17:31:39 +0200

    add pictures for basile and marie
    # Conflicts:
    #	projects/cc/src/app/user/user.bogus.ts

--- projects/cc/src/app/user/user.bogus.ts
+++ projects/cc/src/app/user/user.bogus.ts
@@ -3,23 +3,27 @@ export const basile = {
 	id: 432,
 	firstName: 'Basile',
 	lastName: 'Minvielle',
-	picture: {},
+	jobTitle: 'customer success guru',
+	picture: {
+		href: 'https://www.lucca.fr/img/equipe/bminvielle.jpg',
+	}
 } as IUser;
 export const marie = {
 	id: 509,
 	firstName: 'Marie',
 	lastName: 'Lambert',
-	picture: {},
+	jobTitle: 'bizdev wow gal',
+	picture: {
+		href: 'https://www.lucca.fr/img/equipe/mlambert.jpg',
+	}
 } as IUser;
 export const hugo = {
 	id: 493,
 	firstName: 'Hugo',
 	lastName: 'Barbagelata',
-	picture: {},
 } as IUser;
 export const robin = {
 	id: 474,
 	firstName: 'Robin',
 	lastName: 'Le Gales',
-	picture: {},
 } as IUser;

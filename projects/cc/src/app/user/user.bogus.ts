db96eac12fa9cd5c8d00e632e91e70211142abe1
lucienbertin <me@lucienbertin.com>
2018-06-26 15:17:52 +0200

    add some users in /user

---
+++ projects/cc/src/app/user/user.bogus.ts
@@ -0,0 +1,25 @@
+import { IUser } from '@lucca-front/ng';
+export const basile = {
+	id: 432,
+	firstName: 'Basile',
+	lastName: 'Minvielle',
+	picture: {},
+} as IUser;
+export const marie = {
+	id: 509,
+	firstName: 'Marie',
+	lastName: 'Lambert',
+	picture: {},
+} as IUser;
+export const hugo = {
+	id: 493,
+	firstName: 'Hugo',
+	lastName: 'Barbagelata',
+	picture: {},
+} as IUser;
+export const robin = {
+	id: 474,
+	firstName: 'Robin',
+	lastName: 'Le Gales',
+	picture: {},
+} as IUser;

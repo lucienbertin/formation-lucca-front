7e47a02b7ea5dcbdd59cb020a457e10ad3042618
lucienbertin <me@lucienbertin.com>
2018-06-26 16:28:16 +0200

    add jobtitle to users

--- projects/cc/src/app/user/user.bogus.ts
+++ projects/cc/src/app/user/user.bogus.ts
@@ -3,23 +3,23 @@ export const basile = {
 	id: 432,
 	firstName: 'Basile',
 	lastName: 'Minvielle',
-	picture: {},
+	jobTitle: 'customer success guru',
 } as IUser;
 export const marie = {
 	id: 509,
 	firstName: 'Marie',
 	lastName: 'Lambert',
-	picture: {},
+	jobTitle: 'bizdev wow gal',
 } as IUser;
 export const hugo = {
 	id: 493,
 	firstName: 'Hugo',
 	lastName: 'Barbagelata',
-	picture: {},
+	jobTitle: 'customer success wow guy',
 } as IUser;
 export const robin = {
 	id: 474,
 	firstName: 'Robin',
 	lastName: 'Le Gales',
-	picture: {},
+	jobTitle: 'bizdev wow guy',
 } as IUser;
